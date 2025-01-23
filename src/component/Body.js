e7603587fd34c58a984789ded1253ee31e616b00
meshpuja <meshpuja@gmail.com>
2025-01-23 10:54:06 +0530

    vercel proxy added

--- src/component/Body.js
+++ src/component/Body.js
@@ -19,8 +19,9 @@ const Body = () => {
   const RestroCardPromted = withPromoted(RestroCard);
   const fetchData = async () => {
     const resList = await fetch(
-      "https://thingproxy.freeboard.io/fetch/" +
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0547641&lng=72.9102511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://cors-by-codethread-for-swiggy.vercel.app/cors/dapi/restaurants/list/v5?lat=19.8134554&lng=85.8312359&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      //   "https://cors-handlers.vercel.app/" +
+      //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0547641&lng=72.9102511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await resList.json();
     setlistOfRes(
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,4 +3,4 @@ export const IMG_URL =
 export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png/";
 export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0547641&lng=72.9102511&restaurantId=";
+  "https://cors-by-codethread-for-swiggy.vercel.app/cors/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0547641&lng=72.9102511&restaurantId=";
